a8eecd9b295231d4779d65eeec4ddd110dcddb24
Kevin Hoffman <alothien@gmail.com>
2020-10-28 10:28:13 -0400

    adding key value interface

---
+++ rust/keyvalue/src/generated.rs
@@ -0,0 +1,465 @@
+extern crate rmp_serde as rmps;
+use rmps::{Deserializer, Serializer};
+use serde::{Deserialize, Serialize};
+use std::io::Cursor;
+
+
+extern crate wapc_guest as guest;
+use guest::prelude::*;
+
+
+/// The abstraction of the key-value host capability 
+pub struct Host {
+    binding: String,
+}
+
+impl Default for Host {
+    fn default() -> Self {
+        Host {
+            binding: "default".to_string(),
+        }
+    }
+}
+
+/// Creates a named host binding for the key-value store capability
+pub fn host(binding: &str) -> Host {
+    Host {
+        binding: binding.to_string(),
+    }
+}
+
+/// Creates the default host binding for the key-value store capability
+pub fn default() -> Host {
+    Host::default()
+}
+
+impl Host {
+    /// Retrieves a value stored in a given key
+    pub fn get(&self, key: String) -> HandlerResult<GetResponse> {
+        let input_args = GetArgs { key: key };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "Get",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<GetResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Adds a number to the value stored at a given key. Will return an error
+    /// if you attempt to add a value to an existing key that is holding a string
+    pub fn add(&self, key: String, value: i32) -> HandlerResult<AddResponse> {
+        let input_args = AddArgs {
+            key: key,
+            value: value,
+        };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "Add",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<AddResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Sets the given value for the key with an optional expiration period. Set expiration to 0
+    /// for a value that does not expire.
+    pub fn set(&self, key: String, value: String, expires: i32) -> HandlerResult<SetResponse> {
+        let input_args = SetArgs {
+            key: key,
+            value: value,
+            expires: expires,
+        };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "Set",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<SetResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Deletes the specified key
+    pub fn del(&self, key: String) -> HandlerResult<DelResponse> {
+        let input_args = DelArgs { key: key };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "Del",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<DelResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Clears the list at the given key 
+    pub fn clear(&self, key: String) -> HandlerResult<DelResponse> {
+        let input_args = ClearArgs { key: key };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "Clear",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<DelResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Retrieves a range of values stored in a list key
+    pub fn range(&self, key: String, start: i32, stop: i32) -> HandlerResult<ListRangeResponse> {
+        let input_args = RangeArgs {
+            key: key,
+            start: start,
+            stop: stop,
+        };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "Range",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<ListRangeResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Pushes a value onto a list
+    pub fn push(&self, key: String, value: String) -> HandlerResult<ListResponse> {
+        let input_args = PushArgs {
+            key: key,
+            value: value,
+        };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "Push",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<ListResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Deletes the given item from a specified list
+    pub fn list_item_delete(&self, key: String, value: String) -> HandlerResult<ListResponse> {
+        let input_args = ListItemDeleteArgs {
+            key: key,
+            value: value,
+        };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "ListItemDelete",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<ListResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Adds a value to a set
+    pub fn set_add(&self, key: String, value: String) -> HandlerResult<SetOperationResponse> {
+        let input_args = SetAddArgs {
+            key: key,
+            value: value,
+        };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "SetAdd",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<SetOperationResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Removes an item from a set
+    pub fn set_remove(&self, key: String, value: String) -> HandlerResult<SetOperationResponse> {
+        let input_args = SetRemoveArgs {
+            key: key,
+            value: value,
+        };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "SetRemove",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<SetOperationResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Returns the union of all sets specified by the list of keys
+    pub fn set_union(&self, keys: Vec<String>) -> HandlerResult<SetQueryResponse> {
+        let input_args = SetUnionArgs { keys: keys };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "SetUnion",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<SetQueryResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Returns the intersection of all sets specified by the list of keys
+    pub fn set_intersection(&self, keys: Vec<String>) -> HandlerResult<SetQueryResponse> {
+        let input_args = SetIntersectionArgs { keys: keys };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "SetIntersection",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<SetQueryResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Returns the list of members in a set
+    pub fn set_query(&self, key: String) -> HandlerResult<SetQueryResponse> {
+        let input_args = SetQueryArgs { key: key };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "SetQuery",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<SetQueryResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+
+    /// Indicates if a given key exists. The "value" field will be empty in this response
+    pub fn key_exists(&self, key: String) -> HandlerResult<GetResponse> {
+        let input_args = KeyExistsArgs { key: key };
+        host_call(
+            &self.binding,
+            "wascc:keyvalue",
+            "KeyExists",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<GetResponse>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+}
+
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct GetArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct AddArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+    #[serde(rename = "value")]
+    pub value: i32,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct SetArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+    #[serde(rename = "value")]
+    pub value: String,
+    #[serde(rename = "expires")]
+    pub expires: i32,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct DelArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct ClearArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct RangeArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+    #[serde(rename = "start")]
+    pub start: i32,
+    #[serde(rename = "stop")]
+    pub stop: i32,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct PushArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+    #[serde(rename = "value")]
+    pub value: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct ListItemDeleteArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+    #[serde(rename = "value")]
+    pub value: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct SetAddArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+    #[serde(rename = "value")]
+    pub value: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct SetRemoveArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+    #[serde(rename = "value")]
+    pub value: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct SetUnionArgs {
+    #[serde(rename = "keys")]
+    pub keys: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct SetIntersectionArgs {
+    #[serde(rename = "keys")]
+    pub keys: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct SetQueryArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct KeyExistsArgs {
+    #[serde(rename = "key")]
+    pub key: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct GetResponse {
+    #[serde(rename = "value")]
+    pub value: String,
+    #[serde(rename = "exists")]
+    pub exists: bool,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct AddResponse {
+    #[serde(rename = "value")]
+    pub value: i32,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct DelResponse {
+    #[serde(rename = "key")]
+    pub key: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct ListRangeResponse {
+    #[serde(rename = "values")]
+    pub values: Vec<String>,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct ListResponse {
+    #[serde(rename = "newCount")]
+    pub new_count: i32,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct SetResponse {
+    #[serde(rename = "value")]
+    pub value: String,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct SetOperationResponse {
+    #[serde(rename = "new_count")]
+    pub new_count: i32,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct SetQueryResponse {
+    #[serde(rename = "values")]
+    pub values: Vec<String>,
+}
+
+/// The standard function for serializing codec structs into a format that can be
+/// used for message exchange between actor and host. Use of any other function to
+/// serialize could result in breaking incompatibilities.
+pub fn serialize<T>(
+    item: T,
+) -> ::std::result::Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>>
+where
+    T: Serialize,
+{
+    let mut buf = Vec::new();
+    item.serialize(&mut Serializer::new(&mut buf).with_struct_map())?;
+    Ok(buf)
+}
+
+/// The standard function for de-serializing codec structs from a format suitable
+/// for message exchange between actor and host. Use of any other function to
+/// deserialize could result in breaking incompatibilities.
+pub fn deserialize<'de, T: Deserialize<'de>>(
+    buf: &[u8],
+) -> ::std::result::Result<T, Box<dyn std::error::Error + Send + Sync>> {
+    let mut de = Deserializer::new(Cursor::new(buf));
+    match Deserialize::deserialize(&mut de) {
+        Ok(t) => Ok(t),
+        Err(e) => Err(format!("Failed to de-serialize: {}", e).into()),
+    }
+}
---
+++ rust/keyvalue/src/lib.rs
@@ -0,0 +1,24 @@
+#![doc(html_logo_url = "https://avatars2.githubusercontent.com/u/52050279?s=200&v=4")]
+//! # waSCC Key-Value Store Actor Interface
+//!
+//! This crate provides waSCC actors with an interface to the key-value capability provider. Actors using this
+//! interface must have the claim `wascc:key_value` in order to have permission to communicate with the store.
+//!
+//! The key-value provider is one-way, and only accepts host calls from the actor. This provider does _not_
+//! deliver messages to actors.
+//!
+//! # Example:
+//! ```
+//! extern crate actor_keyvalue as kv;
+//! use wapc_guest::HandlerResult;
+//!
+//! fn add() -> HandlerResult<()> {
+//!   let _ = kv::default().add("test".to_string(), 1)?;    
+//!   Ok(())
+//! }
+//! ```
+//!
+
+mod generated;
+
+pub use generated::*;
