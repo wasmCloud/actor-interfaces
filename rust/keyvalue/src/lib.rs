d39f626cf7414d9785f9889a61cf51c7f1cf2001
Kevin Hoffman <alothien@gmail.com>
2020-12-14 10:52:17 -0500

    Renaming the contract ID for keyvalue

--- rust/keyvalue/src/lib.rs
+++ rust/keyvalue/src/lib.rs
@@ -1,8 +1,8 @@
 #![doc(html_logo_url = "https://avatars2.githubusercontent.com/u/52050279?s=200&v=4")]
-//! # waSCC Key-Value Store Actor Interface
+//! # wasmCloud Key-Value Store Actor Interface
 //!
-//! This crate provides waSCC actors with an interface to the key-value capability provider. Actors using this
-//! interface must have the claim `wascc:key_value` in order to have permission to communicate with the store.
+//! This crate provides wasmCloud actors with an interface to the key-value capability provider. Actors using this
+//! interface must have the claim `wasmcloud:keyvalue` in order to have permission to communicate with the store.
 //!
 //! The key-value provider is one-way, and only accepts host calls from the actor. This provider does _not_
 //! deliver messages to actors.
