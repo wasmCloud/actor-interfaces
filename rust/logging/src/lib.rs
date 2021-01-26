da00b7f8b0e4e71f082a4fd4e213691ae74f0298
Brooks Townsend <brooksmtownsend@gmail.com>
2021-01-26 11:34:27 -0500

    included sample in rustdoc

--- rust/logging/src/lib.rs
+++ rust/logging/src/lib.rs
@@ -5,18 +5,20 @@
 //! This crate provides an abstraction over the `wasmcloud:logging` contract. This
 //! allows actors to use normal log macros (like `info!`, `warn!`, `error!`, etc)
 //! to write logs from within the actor.
-
-// extern crate wapc_guest as guest;
-// use guest::prelude::*;
-
-// #[no_mangle]
-// pub fn wapc_init() {
-//     Handlers::register_write_log(write_log);
-// }
-
-// fn write_log(_request: WriteLogRequest) -> HandlerResult<()> {
-//     Ok(()) // TODO: Provide implementation.
-// }
+//!
+//! ```rust
+//! extern crate wapc_guest as guest;
+//! use guest::prelude::*;
+//!
+//! #[no_mangle]
+//! pub fn wapc_init() {
+//!     Handlers::register_write_log(write_log);
+//! }
+//!
+//! fn write_log(_request: WriteLogRequest) -> HandlerResult<()> {
+//!     Ok(()) // TODO: Provide implementation.
+//! }
+//! ```
 
 pub mod generated;
 #[allow(unused_imports)]
