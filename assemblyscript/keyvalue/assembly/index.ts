faed073b9265db5aacf20fce5d536701e3e760ae
Brooks Townsend <brooksmtownsend@gmail.com>
2021-01-26 15:38:27 -0500

    Updated AS metadata for npm publishes

--- assemblyscript/keyvalue/assembly/index.ts
+++ assemblyscript/keyvalue/assembly/index.ts
@@ -8,19 +8,4 @@
   * deliver messages to actors.
  */
 
-// Boilerplate code for waPC.  Do not remove.
-import { handleCall, handleAbort } from "@wapc/as-guest";
-
-export function __guest_call(operation_size: usize, payload_size: usize): bool {
-  return handleCall(operation_size, payload_size);
-}
-
-// Abort function
-function abort(
-  message: string | null,
-  fileName: string | null,
-  lineNumber: u32,
-  columnNumber: u32
-): void {
-  handleAbort(message, fileName, lineNumber, columnNumber);
-}
+export * from "./module";
