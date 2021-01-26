19b421e8182ab2a64b7f5cb842b1026428e9c63b
Brooks Townsend <brooksmtownsend@gmail.com>
2021-01-26 11:37:19 -0500

    Removed extraneous comments, removed crates from AS modules

--- assemblyscript/http-server/assembly/index.ts
+++ assemblyscript/http-server/assembly/index.ts
@@ -1,24 +1,13 @@
 /**
  * HTTP Server wasmCloud Actor Interface
  * 
- * This crate provides wasmCloud actors with an interface to the HTTP Server capability provider. Actors using this
- * interface must have the claim `wascc:http_server` in order to have permission to handle requests, and they
+ * This module provides wasmCloud actors with an interface to the HTTP Server capability provider. Actors using this
+ * interface must have the claim `wasmcloud:httpserver` in order to have permission to handle requests, and they
  * must have an active, configured binding to an HTTP Server capability provider.
  *
  * The HTTP Server provider is one-way, and only delivers messages to actors. Actors cannot make host calls
  * to this provider.
  */
-
-
-// import { Request, Response, Handlers } from "./module";
-
-// export function wapc_init(): void {
-//   Handlers.registerHandleRequest(HandleRequest);
-// }
-
-// function HandleRequest(request: Request): Response {
-//   return new Response(); // TODO: Provide implementation.
-// }
 
 // Boilerplate code for waPC.  Do not remove.
 import { handleCall, handleAbort } from "@wapc/as-guest";
--- assemblyscript/keyvalue/assembly/index.ts
+++ assemblyscript/keyvalue/assembly/index.ts
@@ -1,97 +1,12 @@
 /**
   * wasmCloud Key-Value Store Actor Interface
   * 
-  * This crate provides wasmCloud actors with an interface to the key-value capability provider. Actors using this
+  * This module provides wasmCloud actors with an interface to the key-value capability provider. Actors using this
   * interface must have the claim `wasmcloud:keyvalue` in order to have permission to communicate with the store.
   * 
   * The key-value provider is one-way, and only accepts host calls from the actor. This provider does _not_
   * deliver messages to actors.
  */
-
-// import {
-//   GetResponse,
-//   AddResponse,
-//   DelResponse,
-//   ListRangeResponse,
-//   ListResponse,
-//   SetResponse,
-//   SetOperationResponse,
-//   SetQueryResponse,
-//   Handlers,
-// } from "./module";
-
-// export function wapc_init(): void {
-//   Handlers.registerGet(Get);
-//   Handlers.registerAdd(Add);
-//   Handlers.registerSet(Set);
-//   Handlers.registerDel(Del);
-//   Handlers.registerClear(Clear);
-//   Handlers.registerRange(Range);
-//   Handlers.registerPush(Push);
-//   Handlers.registerListItemDelete(ListItemDelete);
-//   Handlers.registerSetAdd(SetAdd);
-//   Handlers.registerSetRemove(SetRemove);
-//   Handlers.registerSetUnion(SetUnion);
-//   Handlers.registerSetIntersection(SetIntersection);
-//   Handlers.registerSetQuery(SetQuery);
-//   Handlers.registerKeyExists(KeyExists);
-// }
-
-// function Get(key: string): GetResponse {
-//   return new GetResponse(); // TODO: Provide implementation.
-// }
-
-// function Add(key: string, value: i32): AddResponse {
-//   return new AddResponse(); // TODO: Provide implementation.
-// }
-
-// function Set(key: string, value: string, expires: i32): SetResponse {
-//   return new SetResponse(); // TODO: Provide implementation.
-// }
-
-// function Del(key: string): DelResponse {
-//   return new DelResponse(); // TODO: Provide implementation.
-// }
-
-// function Clear(key: string): DelResponse {
-//   return new DelResponse(); // TODO: Provide implementation.
-// }
-
-// function Range(key: string, start: i32, stop: i32): ListRangeResponse {
-//   return new ListRangeResponse(); // TODO: Provide implementation.
-// }
-
-// function Push(key: string, value: string): ListResponse {
-//   return new ListResponse(); // TODO: Provide implementation.
-// }
-
-// function ListItemDelete(key: string, value: string): ListResponse {
-//   return new ListResponse(); // TODO: Provide implementation.
-// }
-
-// function SetAdd(key: string, value: string): SetOperationResponse {
-//   return new SetOperationResponse(); // TODO: Provide implementation.
-// }
-
-// function SetRemove(key: string, value: string): SetOperationResponse {
-//   return new SetOperationResponse(); // TODO: Provide implementation.
-// }
-
-// function SetUnion(keys: Array<string>): SetQueryResponse {
-//   return new SetQueryResponse(); // TODO: Provide implementation.
-// }
-
-// function SetIntersection(keys: Array<string>): SetQueryResponse {
-//   return new SetQueryResponse(); // TODO: Provide implementation.
-// }
-
-// function SetQuery(key: string): SetQueryResponse {
-//   return new SetQueryResponse(); // TODO: Provide implementation.
-// }
-
-// function KeyExists(key: string): GetResponse {
-//   return new GetResponse(); // TODO: Provide implementation.
-// }
 
 // Boilerplate code for waPC.  Do not remove.
 import { handleCall, handleAbort } from "@wapc/as-guest";
