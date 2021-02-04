d5dd5fed372e332758c37d271778703ef8d204cc
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2021-02-04 11:30:04 -0500

    Golang (#36)
    * Initial folder re-organization
    
    * Rearrange, merge, and Rust implementation of extras
    
    * Preventing accidental re-generation of code
    
    * Adding tinygo actor-core and updating rustdocs on extras
    
    * Fixing go module and adding README for actor-core tinygo
    
    * Removing superfluous schema
    
    * Module name tweaks per Phil
    
    * more tweaks

---
+++ actor-core/go/README.md
@@ -0,0 +1,25 @@
+# Actor Core wasmCloud Interface (TinyGo)
+
+This is the actor interface for the `wasmcloud:core` capability contract ID. This interface 
+is required by all actors. Using this interface your actor can implement a handler for
+the health check message.
+
+```go
+package main
+
+import (
+	core "github.com/wasmcloud/actor-interfaces/actor-core/go"
+)
+
+func main() {
+	core.Handlers{
+		HealthRequest: healthRequest,
+	}.Register()
+}
+
+func healthRequest(request core.HealthCheckRequest) (core.HealthCheckResponse, error) {
+	return core.HealthCheckResponse{
+        healthy: true
+    }, nil // TODO: Provide implementation.
+}
+```
--- extras/rust/src/lib.rs
+++ extras/rust/src/lib.rs
@@ -10,6 +10,42 @@
 //! * [request_guid](generated::Host::request_guid)
 //! * [request_sequence](generated::Host::request_sequence)
 //! * [request_random](generated::Host::request_random)
+//!
+//! Example:
+//!
+//! ```rust
+//! extern crate wapc_guest as guest;
+//! use guest::prelude::*;
+//! use actor_extras as extras;
+//! use actor_http_server as http;
+//! use serde_json::json;
+//! use log::{error, info};
+//!
+//! #[no_mangle]
+//! pub fn wapc_init() {
+//!     http::Handlers::register_handle_request(generate_guid);
+//!     actor_core::Handlers::register_health_request(health);
+//! }
+//!
+//! /// Generate a Guid and return it in a JSON envelope
+//! fn generate_guid(_req: http::Request) -> HandlerResult<http::Response> {
+//!   let guid = get_guid()?      // Replace this with `extras::default().request_guid()?`
+//!                 .unwrap_or("unknown-guid".to_string());
+//!
+//!   let result = json!({"guid": guid });
+//!   Ok(http::Response::json(&result, 200, "OK"))
+//!
+//! }
+//!
+//! fn health(_: actor_core::HealthCheckRequest) -> HandlerResult<actor_core::HealthCheckResponse> {
+//!   Ok(actor_core::HealthCheckResponse::healthy())   
+//! }
+//!
+//! # fn get_guid() -> HandlerResult<Option<String>> {
+//! #   Ok(Some("test".to_string()))
+//! # }
+//!
+//! ```
 
 mod generated;
 #[allow(unused_imports)]
