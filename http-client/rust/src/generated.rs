44f98d73683d42feb5a1abd60272ed8e5c308b7f
Brooks Townsend <brooksmtownsend@gmail.com>
2021-02-08 09:46:48 -0500

    Rust httpclient interface (#37)
    * implemented httpclient interface
    
    * addressed PR comments
    
    * addressed PR comments, fixed warnings for unused dependencies

---
+++ http-client/rust/src/generated.rs
@@ -0,0 +1,123 @@
+extern crate rmp_serde as rmps;
+use rmps::{Deserializer, Serializer};
+use serde::{Deserialize, Serialize};
+use std::io::Cursor;
+
+#[cfg(feature = "guest")]
+extern crate wapc_guest as guest;
+#[cfg(feature = "guest")]
+use guest::prelude::*;
+
+#[cfg(feature = "guest")]
+pub struct Host {
+    binding: String,
+}
+
+#[cfg(feature = "guest")]
+impl Default for Host {
+    fn default() -> Self {
+        Host {
+            binding: "default".to_string(),
+        }
+    }
+}
+
+/// Creates a named host binding
+#[cfg(feature = "guest")]
+pub fn host(binding: &str) -> Host {
+    Host {
+        binding: binding.to_string(),
+    }
+}
+
+/// Creates the default host binding
+#[cfg(feature = "guest")]
+pub fn default() -> Host {
+    Host::default()
+}
+
+#[cfg(feature = "guest")]
+impl Host {
+    /// Make an HTTP request with specified method, headers and body to url.
+    /// This request must be carried out by an appropriately bound capability
+    /// provider implementing `wasmcloud:httpclient`, the request is not
+    /// made directly by the actor.
+    pub fn request(
+        &self,
+        method: String,
+        url: String,
+        headers: std::collections::HashMap<String, String>,
+        body: Vec<u8>,
+    ) -> HandlerResult<Response> {
+        let input_args = RequestArgs {
+            method,
+            url,
+            headers,
+            body,
+        };
+        host_call(
+            &self.binding,
+            "wasmcloud:httpclient",
+            "Request",
+            &serialize(input_args)?,
+        )
+        .map(|vec| {
+            let resp = deserialize::<Response>(vec.as_ref()).unwrap();
+            resp
+        })
+        .map_err(|e| e.into())
+    }
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct RequestArgs {
+    #[serde(rename = "method")]
+    pub method: String,
+    #[serde(rename = "url")]
+    pub url: String,
+    #[serde(rename = "headers")]
+    pub headers: std::collections::HashMap<String, String>,
+    #[serde(with = "serde_bytes")]
+    #[serde(rename = "body")]
+    pub body: Vec<u8>,
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize, Default, Clone)]
+pub struct Response {
+    #[serde(rename = "statusCode")]
+    pub status_code: u32,
+    #[serde(rename = "status")]
+    pub status: String,
+    #[serde(rename = "header")]
+    pub header: std::collections::HashMap<String, String>,
+    #[serde(with = "serde_bytes")]
+    #[serde(rename = "body")]
+    pub body: Vec<u8>,
+}
+
+/// The standard function for serializing codec structs into a format that can be
+/// used for message exchange between actor and host. Use of any other function to
+/// serialize could result in breaking incompatibilities.
+pub fn serialize<T>(
+    item: T,
+) -> ::std::result::Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>>
+where
+    T: Serialize,
+{
+    let mut buf = Vec::new();
+    item.serialize(&mut Serializer::new(&mut buf).with_struct_map())?;
+    Ok(buf)
+}
+
+/// The standard function for de-serializing codec structs from a format suitable
+/// for message exchange between actor and host. Use of any other function to
+/// deserialize could result in breaking incompatibilities.
+pub fn deserialize<'de, T: Deserialize<'de>>(
+    buf: &[u8],
+) -> ::std::result::Result<T, Box<dyn std::error::Error + Send + Sync>> {
+    let mut de = Deserializer::new(Cursor::new(buf));
+    match Deserialize::deserialize(&mut de) {
+        Ok(t) => Ok(t),
+        Err(e) => Err(format!("Failed to de-serialize: {}", e).into()),
+    }
+}
---
+++ http-client/rust/src/lib.rs
@@ -0,0 +1,63 @@
+#![doc(html_logo_url = "https://avatars2.githubusercontent.com/u/52050279?s=200&v=4")]
+//! # HTTP Client wasmCloud Actor Interface
+//!
+//! This crate provides wasmCloud actors with an interface to the HTTP client capability provider. Actors using this
+//! interface must have the claim `wasmcloud:httpclient` in order to have permission to make outbound HTTP requests,
+//! and they must have an active, configured binding to an HTTP Client capability provider.
+//!
+//! wasmCloud actors without this permission and capability binding will be unable to make outbound HTTP requests.
+//!
+//! # Example:
+//! ```
+//! use wapc_guest::HandlerResult;
+//! extern crate actor_http_server as httpserver;
+//! extern crate actor_http_client as httpclient;
+//! extern crate actor_core as core;
+//!
+//! const API_URL: &str = "https://wasmcloudapi.cloud.io/proxy";
+//!
+//! #[no_mangle]
+//! pub fn wapc_init() {
+//!     httpserver::Handlers::register_handle_request(get_proxy);
+//!     core::Handlers::register_health_request(health);
+//! }
+//!
+//! fn health(_: core::HealthCheckRequest) -> HandlerResult<core::HealthCheckResponse> {
+//!   Ok(core::HealthCheckResponse::healthy())   
+//! }
+//!
+//! /// This function proxys an inbound HTTP request to an external server
+//! fn get_proxy(msg: httpserver::Request) -> HandlerResult<httpserver::Response> {
+//!     // Form client request from server request
+//!     if msg.method == "GET".to_string() {
+//!         // Replace `request` with `httpclient::default().request`
+//!         let res = request(msg.method, API_URL.to_string(), msg.header)?;
+//!         // Form server response
+//!         Ok(httpserver::Response {
+//!             status_code: res.status_code,
+//!             status: res.status,
+//!             header: res.header,
+//!             body: res.body,
+//!         })
+//!     } else {
+//!         Ok(httpserver::Response::internal_server_error("Only GET requests can be proxied with this actor"))
+//!     }
+//! }
+//!
+//! # fn request(method: String, url: String, headers: std::collections::HashMap<String,String>) -> HandlerResult<httpclient::Response> {
+//! #   Ok(httpclient::Response {
+//! #     status: "OK".to_string(),
+//! #     status_code: 200,
+//! #     ..Default::default()
+//! #   })
+//! # }
+
+#[cfg(feature = "guest")]
+extern crate wapc_guest as guest;
+#[cfg(feature = "guest")]
+#[allow(unused)]
+use guest::prelude::*;
+mod generated;
+pub use generated::*;
+
+pub const OP_PERFORM_REQUEST: &str = "PerformRequest";
